8daceb225f6632417e0cd7d7fbe41ad813161680
admbalogh <baloghadam91@icloud.com>
2022-10-08 14:45:22 +0100

    GitHub repository link added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <p>
+        Click on to see the{" "}
+        <a
+          href="https://github.com/admbalogh/weatherapp"
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
